2b5352729fc7ea3fc43f1fb6aa3ba780c95c16c5
kritika-2006 <kritika02056@gmail.com>
2026-03-20 21:16:20 +0530

    main

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+a = "kritika";
+b =  4;
+c =  3.14;
+d =  true;
+e =  'w';
+
+console.log(a,typeof(a));
+console.log(b,typeof(b));
+console.log(c,typeof(c));
+console.log(d,typeof(d));
+console.log(e,typeof(e));
