5089e47f9356800e24bd84f4bccb8a32a5276b1b
kritika-2006 <kritika02056@gmail.com>
2026-03-20 20:57:40 +0530

    main

---
+++ .dist/rating.js
@@ -0,0 +1,17 @@
+rating = 5;
+//1 - poor
+//2 - Below average
+//3 - Average
+//4 - Good
+//5 - Excellent
+
+if (rating==1)
+console.log("poor");
+else if (rating==2)
+    console.log("Below average");
+else if (rating==3)
+    console.log("Average");
+else if (rating==4)
+    console.log("Good");
+else 
+    console.log("Excellent");
