540de194133af89799f572767391c3de9503be1d
kritika-2006 <kritika02056@gmail.com>
2026-03-23 14:16:25 +0530

    main

---
+++ string.js
@@ -0,0 +1,11 @@
+let s ="kritika";
+console.log(s,s.length);
+// for loop
+//for(let i=0;i<s.length;i++){
+  //  console.log(s[i]);
+//}
+// forOf loop
+for (const char of s) {
+    console.log(char);
+}
+// forEach loop does not work for string
