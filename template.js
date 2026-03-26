ac5f5bf1a2367cabff45983fa0a6dcde353c569f
kritika-2006 <kritika02056@gmail.com>
2026-03-26 19:27:10 +0530

    update

--- template.js
+++ template.js
@@ -1,6 +1,9 @@
 let age = 19;
 let exp = 5;
 console.log(`My age is ${age} and my experience is ${exp}`);
-// use of backtick (~)
+// use of backtick (~) and singletick (')
+// print krit"ika
 console.log(`krit"ika`);
 console.log("krit\"ika"); 
+console.log('krit"ika');
+console.log(`krit'ika`);
